8b1f548f03afdaa855955284f80ae608c2bfdde1
moznion <moznion@gmail.com>
2012-09-12 18:06:32 +0900

    Initial Commit.

---
+++ Zappa.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/env perl
+
+use 5.016;
+
+use utf8;
+use FindBin;
+use lib ("$FindBin::Bin/lib");
+
+use MIME::Base64;
+use Mojolicious::Lite;
+use Zappa::Github;
+
+my $github = Zappa::Github->new;
+
+get '/' => 'index';
+
+
+# REPOS
+get '/repos/:user' => sub {
+  my $self = shift;
+  $self->stash(github => $github);
+
+  $self->render('repos_list');
+};
+
+get '/repos/:user/*repos' => sub {
+  my $self = shift;
+  $self->stash(github => $github);
+
+  $self->render('repos_contents');
+};
+
+
+# FILE
+get '/file/:user/:repos/*path' => sub {
+  my $self = shift;
+
+  my %contents = $github->fetch_file_contents(
+    $self->stash->{'user'},
+    $self->stash->{'repos'},
+    $self->stash->{'path'}
+  );
+
+  $self->stash->{'path'} =~ /^(?:.*)\/(.*\.(.*))$/;
+  my $filename  = $1;
+  my $file_type = $2;
+  if ($file_type =~ /jpg|jpeg|gif|png/i) {
+    $self->stash(image    => $contents{'content'});
+    $self->stash(filename => $filename);
+    if ($file_type =~ /jpg|jpeg/i) {
+      $self->stash(image_type => 'jpeg');
+    } else {
+      $self->stash(image_type => $file_type);
+    }
+    $self->render('image');
+  } else {
+    $contents{'content'} = MIME::Base64::decode($contents{'content'});
+    $contents{'content'} =~ s/\r?\n/<br>/g;
+    $self->render(text => $contents{'content'});
+  }
+};
+
+app->start;
+
+__END__
+
+__DATA__
---
+++ lib/Zappa/Github.pm
@@ -0,0 +1,87 @@
+# CONFORMING TO Github API v3
+
+package Zappa::Github;
+use strict;
+use warnings;
+
+use JSON;
+use Furl;
+use URI;
+use Carp;
+use Try::Tiny;
+use Class::Load ':all';
+
+sub new {
+  my ($class, %opts) = @_;
+
+  $opts{base_uri} ||= 'https://api.github.com/';
+  $opts{parser}   ||= 'JSON';
+  $opts{agent}    ||= Furl->new(
+    agent   => $class,
+    timeout => 10,
+  );
+  return bless {%opts}, $class;
+}
+
+sub get {
+  my ($self, $path) = @_;
+
+  my $uri = URI->new($self->{base_uri});
+  $uri->path($path);
+  my $response = $self->{agent}->get($uri);
+  Carp::croak(sprintf "%s(code:%s)", $response->content, $response->code) unless $response->is_success;
+
+  return try {
+    JSON->new->utf8->decode($response->content);
+  } catch {
+    Carp::croak(sprintf "%s(uri:%s)", $_, $uri->as_string);
+  }
+}
+
+sub fetch_repos_contents {
+  my ($self, $user, $repos) = @_;
+
+  my $response;
+  if ($repos =~ /^(.*?)\/(.*)/) {
+    $response = $self->get(sprintf "/repos/%s/%s/%s/%s", $user, $1, 'contents', $2);
+  } else {
+    $response = $self->get(sprintf "/repos/%s/%s/%s", $user, $repos, 'contents');
+  }
+  my %directory;
+  foreach my $content (@$response) {
+    my %content_info;
+    $content_info{'name'}     = $content->{'name'};
+    $content_info{'type'}     = $content->{'type'};
+    $content_info{'location'} = $content->{'_links'}->{'self'};
+    $directory{$content->{'path'}} = \%content_info;
+  }
+
+  return %directory;
+}
+
+sub fetch_user_repos {
+  my ($self, $user) = @_;
+
+  my $response = $self->get(sprintf "/users/%s/repos", $user);
+  my %repositories;
+  foreach my $repos (@$response) {
+    my %repos_info;
+    $repos_info{'name'}        = $repos->{'name'};
+    $repos_info{'full_name'}   = $repos->{'full_name'};
+    $repos_info{'description'} = $repos->{'description'};
+    $repos_info{'private'}     = $repos->{'private'};
+    $repos_info{'clone_url'}   = $repos->{'clone_url'};
+    $repos_info{'fork'}        = $repos->{'fork'};
+    $repositories{$repos->{'id'}} = \%repos_info;
+  }
+
+  return %repositories;
+}
+
+sub fetch_file_contents {
+  my ($self, $user_name, $repos_name, $file_path) = @_;
+  my $response = $self->get("repos/$user_name/$repos_name/contents/$file_path");
+  return %$response;
+}
+
+1;
